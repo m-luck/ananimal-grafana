782a249426ab422f0f4478fe746f65a6dba094e2
unknown <michael.j.lukiman@gmail.com>
2021-01-09 12:25:37 -0800

    Update intervals and move to secrets.

--- infl.py
+++ infl.py
@@ -5,44 +5,48 @@ import psutil
 import time
 from influxdb import InfluxDBClient
 
+import secrets as sec
+
 # influx configuration - edit these
-ifuser = "grafana"
-ifpass = "<yourpassword>"
+ifuser = sec.gr_user
+ifpass = sec.gr_pw
 ifdb   = "home"
-ifhost = "127.0.0.1"
-ifport = 8086
+ifhost = sec.gr_host
+ifport = sec.gr_port
 measurement_name = "system"
-
-# take a timestamp for this measurement
-curr_time = datetime.datetime.utcnow()
-
-# collect some stats from psutil
-disk = psutil.disk_usage('/')
-mem = psutil.virtual_memory()
-load = psutil.cpu_percent()
-
-# format the data as a single measurement for influx
-body = [
-    {
-        "measurement": measurement_name,
-        "time": curr_time,
-        
-        "fields": {
-            "load": load,
-            "disk_percent": disk.percent,
-            "disk_free": disk.free,
-            "disk_used": disk.used,
-            "mem_percent": mem.percent,
-            "mem_free": mem.free,
-            "mem_used": mem.used,
-        }
-    }
-]
 
 # connect to influx
 ifclient = InfluxDBClient(ifhost,ifport,ifuser,ifpass,ifdb)
 
 # write the measurement
 while True:
+
+    # take a timestamp for this measurement
+    curr_time = datetime.datetime.utcnow()
+
+    # collect some stats from psutil
+    disk = psutil.disk_usage('/')
+    mem = psutil.virtual_memory()
+    load = psutil.cpu_percent()
+
+    # format the data as a single measurement for influx
+    body = [
+        {
+            "measurement": measurement_name,
+            "time": curr_time,
+            
+            "fields": {
+                "load": load,
+                "disk_percent": disk.percent,
+                "disk_free": disk.free,
+                "disk_used": disk.used,
+                "mem_percent": mem.percent,
+                "mem_free": mem.free,
+                "mem_used": mem.used,
+            }
+        }
+    ]
+
     time.sleep(5)
+
     ifclient.write_points(body)
